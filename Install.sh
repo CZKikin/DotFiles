d94013f62ffd49845ba1982bd8279dc8696f601f
Kikin <56359940+CZKikin@users.noreply.github.com>
2020-07-22 17:26:48 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -17,9 +17,20 @@ done
 
 sudo pacman -S freetype2 pkg-config xorg xorg-server xorg-xinit  pavucontrol pulseaudio networkmanager unclutter neomutt picom discord network-manager-applet texlive-most ranger arandr feh gcc pandoc net-tools xclip 
 
-echo You should install fonts that are not included. Fonts needed are: Symbola, Minecraft, Hermit
 echo "Downloads finished... "
 
 mv ~/VimRc ~/git_repos/VimRc
 
 ln -s ~/git_repos/VimRc/.* ~/
+
+wget https://github.com/pcaro90/hermit/raw/master/packages/full-hermit-2.0.tar.gz
+wget https://github.com/FortAwesome/Font-Awesome/raw/master/otfs/Font%20Awesome%205%20Free-Regular-400.otf
+wget https://github.com/FortAwesome/Font-Awesome/raw/master/otfs/Font%20Awesome%205%20Brands-Regular-400.otf
+wget https://github.com/FortAwesome/Font-Awesome/raw/master/otfs/Font%20Awesome%205%20Free-Solid-900.otf
+
+tar -xvzf full-hermit-2.0.tar.gz
+
+mv *.otf ~/.fonts || { mkdir ~/.fonts ; mv *.otf ~/.fonts ;}
+rm -rf Hermit*; rm full-hermit*
+fc-cache -frv
+xrdb .Xresources
