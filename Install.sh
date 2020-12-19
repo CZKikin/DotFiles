b6fe97662c9e3e6979f1943c1b92f72ccbca94ac
CZKikin <krystofsadlik13@seznam.cz>
2020-12-19 00:11:33 +0000

    😪 Repaired the scirpt

--- Install.sh
+++ Install.sh
@@ -1,38 +1,40 @@
 #!/bin/sh
-# sudo pacman -S freetype2 pkg-config xorg xorg-server xorg-xinit  pavucontrol pulseaudio networkmanager unclutter neomutt picom discord network-manager-applet texlive-most ranger arandr feh gcc pandoc net-tools xclip wget 
-# echo "Downloads finished. Downloading Suckless utilities"
-# 
-# cd gitRepos  || { mkdir gitRepos  ; cd gitRepos  ;}
-# git clone https://github.com/CZKikin/mySuckless.git
-# 
-# cd mySuckless
-# 
-# echo "Installing Suckless utilities"
-# for i in *; do
-#   cd $i
-#   sudo make install
-#   cd ..
-# done
-# 
-# echo "Installed. Moving repo to gitRepos"
-# mv ~/DotFiles ~/gitRepos/DotFiles
-# 
-# rm ~/.bashrc
-# cd ~/
-for f in .*; do [ "$f" = ".config" ] || [ "$f" = ".git" ] || ln -s ~/gitRepos/Vimrc/$f; done
+ping google.com -c 4 || echo "No internet connection" ;exit 1
+sudo pacman -S freetype2 pkg-config xorg xorg-server xorg-xinit  pavucontrol pulseaudio networkmanager unclutter neomutt picom discord network-manager-applet texlive-most ranger arandr feh gcc pandoc net-tools xclip wget 
+echo "Downloads finished. Downloading Suckless utilities"
 
-# 
-# echo "Downloading fonts"
-# wget https://github.com/pcaro90/hermit/raw/master/packages/full-hermit-2.0.tar.gz
-# wget https://github.com/FortAwesome/Font-Awesome/raw/master/otfs/Font%20Awesome%205%20Free-Regular-400.otf
-# wget https://github.com/FortAwesome/Font-Awesome/raw/master/otfs/Font%20Awesome%205%20Brands-Regular-400.otf
-# wget https://github.com/FortAwesome/Font-Awesome/raw/master/otfs/Font%20Awesome%205%20Free-Solid-900.otf
-# 
-# tar -xvzf full-hermit-2.0.tar.gz
-# 
-# echo "Instllling fonts"
-# mv *.otf ~/.local/share/fonts || { mkdir ~/.local/share/fonts ; mv *.otf ~/.local/share/fonts ;}
-# rm -rf Hermit*; rm full-hermit*
-# fc-cache -frv
-# 
-# echo "Minecraft is the only missing font, please install" 
+cd gitRepos  || { mkdir gitRepos  ; cd gitRepos  ;}
+git clone https://github.com/CZKikin/mySuckless.git
+
+cd mySuckless
+
+echo "Installing Suckless utilities"
+for i in *; do
+  cd $i
+  sudo make install
+  cd ..
+done
+
+echo "Installed. Moving repo to gitRepos"
+mv ~/DotFiles ~/gitRepos/DotFiles
+
+rm ~/.bashrc
+cd ~/
+
+for f in .*; do [ "$f" = ".config" ] || [ "$f" = ".git" ] || ln -s ~/gitRepos/DotFiles/$f; done
+ 
+echo "Downloading fonts"
+wget https://github.com/pcaro90/hermit/raw/master/packages/full-hermit-2.0.tar.gz
+wget https://github.com/FortAwesome/Font-Awesome/raw/master/otfs/Font%20Awesome%205%20Free-Regular-400.otf
+wget https://github.com/FortAwesome/Font-Awesome/raw/master/otfs/Font%20Awesome%205%20Brands-Regular-400.otf
+wget https://github.com/FortAwesome/Font-Awesome/raw/master/otfs/Font%20Awesome%205%20Free-Solid-900.otf
+
+tar -xvzf full-hermit-2.0.tar.gz
+
+echo "Instllling fonts"
+mv *.otf ~/.local/share/fonts || { mkdir ~/.local/share/fonts ; mv *.otf ~/.local/share/fonts ;}
+rm -rf Hermit*; rm full-hermit*
+fc-cache -frv
+
+echo "Minecraft is the only missing font, please install" 
+echo "Install your video drivers like xf86-video-amdgpu :)"
